bf66b72cd478b3b519cd6ebd9af937fc4c6386c8
José Veiga <jose.veiga@izii.io>
2023-03-09 23:46:36 -0300

    adicionando componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
-import { Container } from './styles';
+import { Container, LogoImg } from "./styles";
+
+import logoImgRef from "../../assets/logo.png";
 
 const Header = () => {
-  return <Container></Container>;
-}
+  return (
+    <Container>
+      <LogoImg source={logoImgRef} />
+    </Container>
+  );
+};
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,13 @@
-import styled from 'styled-components/native';
-import { gray700 } from '../../assets/styles/variables';
+import styled from "styled-components/native";
+import { gray700 } from "../../assets/styles/variables";
 
 export const Container = styled.View`
   background-color: ${gray700};
-  height: 173px;
+  height: 180px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LogoImg = styled.Image`
+  width: 110px;
 `;
